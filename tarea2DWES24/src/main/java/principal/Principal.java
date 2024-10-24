3b62910261bf77b8ecf138f258b7b99a97ed0b8e
hfm350 <naw02083@educastur.es>
2024-10-24 12:03:00 +0200

    Subir a Git para clonarlo en mi ordenador

--- tarea2DWES24/src/main/java/principal/Principal.java
+++ tarea2DWES24/src/main/java/principal/Principal.java
@@ -1,10 +1,25 @@
 package principal;
 
+import java.util.Scanner;
+
+import entidades.Planta;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in= new Scanner (System.in);
+		System.out.println("Dame el codigo de la nueva planta");
+		String codigo = in.nextLine().trim().toUpperCase();	
+		System.out.println("Dame el nombre común de la nueva planta");
+		String nombrecomun = in.nextLine();
+		System.out.println("Dame el nombre cientifico de la nueva planta");
+		String nombrecientifico = in.nextLine();
+		
+		
+		Planta nueva = new Planta(codigo, nombrecomun, nombrecientifico);
+		
 
+		
 	}
 
 }
